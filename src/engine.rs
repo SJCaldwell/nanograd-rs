73265b51470d93b464ea441c8bd85abd25b3d8f4
Shane Caldlwell <shanejcaldwell@gmail.com>
2024-06-14 14:32:43 -0500

    make a dag in rust they said, itll be fun they said

--- src/engine.rs
+++ src/engine.rs
@@ -1,9 +1,70 @@
-pub struct Value {
+use std::cell::RefCell;
+use std::fmt;
+use std::ops::{Add, Mul};
+use std::rc::Rc;
+
+pub struct _Value {
     pub data: f32,
+    pub _prev: Vec<Rc<RefCell<_Value>>>,
 }
+
+pub struct Value(Rc<RefCell<_Value>>);
 
 impl Value {
     pub fn new(data: f32) -> Self {
-        Value { data: data }
+        Value(Rc::new(RefCell::new(_Value {
+            data,
+            _prev: vec![],
+        })))
+    }
+
+    pub fn new_with_parents(data: f32, parents: Vec<Value>) -> Self {
+        let parent_refs = parents.into_iter().map(|p| p.0.clone()).collect();
+        Value(Rc::new(RefCell::new(_Value {
+            data,
+            _prev: parent_refs,
+        })))
+    }
+
+    pub fn display_parents(&self) -> String {
+        let value_borrow = self.0.borrow();
+        let parents = &value_borrow._prev;
+
+        //Build a string containing all parent values
+        let parent_strings: Vec<String> = parents
+            .iter()
+            .map(|parent| {
+                let parent_borrow = parent.borrow();
+                format!("Value(data={})", parent_borrow.data)
+            })
+            .collect();
+
+        //Join strings
+        parent_strings.join(", ")
+    }
+}
+
+impl Add for Value {
+    type Output = Value;
+
+    fn add(self, other: Self) -> Self::Output {
+        let new_data = self.0.borrow().data + other.0.borrow().data;
+        Value::new_with_parents(new_data, vec![self, other])
+    }
+}
+
+impl Mul for Value {
+    type Output = Value;
+
+    fn mul(self, other: Self) -> Self::Output {
+        let new_data = self.0.borrow().data * other.0.borrow().data;
+        Value::new_with_parents(new_data, vec![self, other])
+    }
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let value_borrow = self.0.borrow();
+        write!(f, "Value(data={})", value_borrow.data)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use nanograd::engine::Value;
 use nanograd::nn::NeuralNetwork;
 fn main() {
     println!("Hello from nanograd, I'm a scalar value autograd engine written in rust!");
+    let a = Value::new(5.0);
+    println!("The value here is: {}", a);
+    let c = a + Value::new(7.0);
+    let e = c * Value::new(10.0);
+    println!("The new value is: {}", e);
 }
