36d643fb7907241f07dc153be8ba9c54481d6860
Shane Caldlwell <shanejcaldwell@gmail.com>
2024-06-15 22:38:50 -0500

    panicking? in my dynamic dispatch computational graph???

--- src/engine.rs
+++ src/engine.rs
@@ -1,7 +1,9 @@
 use std::cell::RefCell;
+use std::collections::HashSet;
 use std::fmt;
 use std::ops::{Add, Div, Mul, Sub};
 use std::rc::Rc;
+
 #[derive(Debug)]
 pub enum Op {
     Init, //Indicates no operation was used to make it
@@ -9,13 +11,15 @@ pub enum Op {
     Mul,
     Div,
     Sub,
+    ReLU,
 }
 
 pub struct _Value {
     pub data: f32,
     pub grad: f32, //gradient used during the background pass
     pub _prev: Vec<Rc<RefCell<_Value>>>,
-    pub op: Op, //Operation that created the value
+    pub op: Op,                          //Operation that created the value
+    _backward: Option<Box<dyn FnMut()>>, //Closure to compute the gradient
 }
 
 pub struct Value(Rc<RefCell<_Value>>);
@@ -27,6 +31,7 @@ impl Value {
             grad: 0.0,
             op: Op::Init,
             _prev: vec![],
+            _backward: None,
         })))
     }
 
@@ -37,7 +42,61 @@ impl Value {
             grad: 0.0,
             op,
             _prev: parent_refs,
+            _backward: None,
         })))
+    }
+
+    pub fn backward(&self) {
+        let mut topo: Vec<Rc<RefCell<_Value>>> = vec![];
+        let mut visited: HashSet<*const _Value> = HashSet::new();
+        let mut stack: Vec<Rc<RefCell<_Value>>> = vec![self.0.clone()];
+
+        while let Some(node) = stack.pop() {
+            let node_ptr = Rc::as_ptr(&node) as *const _Value;
+            if !visited.contains(&node_ptr) {
+                visited.insert(node_ptr);
+                topo.push(node.clone());
+                for parent in &node.borrow()._prev {
+                    stack.push(parent.clone());
+                }
+            }
+        }
+        //Reverse the topological order to start at the root node and go backwards
+        topo.reverse();
+
+        //Initialize the gradient of the output node
+        if let Some(output_node) = topo.first() {
+            output_node.borrow_mut().grad = 1.0;
+        }
+
+        for node in topo {
+            if let Some(ref mut backward_fn) = node.borrow_mut()._backward.take() {
+                backward_fn();
+            }
+        }
+    }
+
+    pub fn relu(&self) -> Value {
+        let data = self.0.borrow().data;
+        let new_data = data.max(0.0); // ReLU function
+        let new_value = Value::new_with_parents(new_data, vec![self.clone()], Op::ReLU);
+
+        // Create a weak reference to new_value and self before defining the closure
+        let new_value_weak = Rc::downgrade(&new_value.0);
+        let self_weak = Rc::downgrade(&self.0);
+
+        // Set up the backward function using only weak references
+        new_value.0.borrow_mut()._backward = Some(Box::new(move || {
+            if let (Some(self_rc), Some(new_value_rc)) =
+                (self_weak.upgrade(), new_value_weak.upgrade())
+            {
+                let mut self_borrow = self_rc.borrow_mut();
+                let new_value_grad = new_value_rc.borrow().grad;
+                self_borrow.grad += if data > 0.0 { new_value_grad } else { 0.0 };
+            }
+        }));
+
+        new_value
     }
 
     pub fn display_parents(&self) -> String {
@@ -57,7 +116,6 @@ impl Value {
         parent_strings.join(", ")
     }
 }
-
 impl Add for Value {
     type Output = Value;
 
@@ -102,5 +160,11 @@ impl fmt::Display for Value {
             "Value(data={}, grad={}, op={:?})",
             value_borrow.data, value_borrow.grad, value_borrow.op
         )
+    }
+}
+
+impl Clone for Value {
+    fn clone(&self) -> Self {
+        Value(self.0.clone())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use nanograd::engine::Value;
 fn main() {
-    println!("Hello from nanograd, I'm a scalar value autograd engine written in rust!");
-    let a = Value::new(5.0);
-    println!("The value here is: {}", a);
-    let c = a + Value::new(7.0);
-    let e = c * Value::new(10.0);
-    println!("The new value is: {}", e);
+    let input = Value::new(2.0);
+    let output = input.relu();
+    output.backward();
 }
